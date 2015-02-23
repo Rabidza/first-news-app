16dbc92fb590ddc38c0716ac2c7a7dc9802a84e7
Neill Henning <neillhenning@gmail.com>
2015-02-23 13:55:34 +0200

    Added 404 page not found flask abort function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask import render_template
 import csv
+from flask import Flask
+from flask import abort
+from flask import render_template
 
 app = Flask(__name__) # Note the double underscores on each side!
 
@@ -25,6 +26,7 @@ def detail(row_id):
     for row in object_list:
         if row['id'] == row_id:
             return render_template(template, object=row)
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
